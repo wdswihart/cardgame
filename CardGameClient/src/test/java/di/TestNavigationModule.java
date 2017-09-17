25beb88e36111e693cecc95dedb8ff9aff8ebeb9
Forrest Dale <forrest.dale@metova.com>
2017-09-17 13:09:23 -0500

    Testing initialization.

---
+++ CardGameClient/src/test/java/di/TestNavigationModule.java
@@ -0,0 +1,12 @@
+package di;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+import core.navigation.INavigationProvider;
+
+public class TestNavigationModule implements Module {
+    @Override
+    public void configure(Binder binder) {
+        binder.bind(INavigationProvider.class).toInstance(TestNavigationProvider.getInstance());
+    }
+}
