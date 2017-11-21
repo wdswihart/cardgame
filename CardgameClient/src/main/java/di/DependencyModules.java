76eba7fdaf4e124cdff7b8a3ea9ba846cc21bf01
William Swihart <wdswihart@gmail.com>
2017-11-20 18:30:43 -0600

    Directory structure fix maybe pt 2

---
+++ CardgameClient/src/main/java/di/DependencyModules.java
@@ -0,0 +1,39 @@
+package di;
+
+import client.core.ConnectionProvider;
+import client.core.ConnectionProviderImpl;
+import client.core.GameProvider;
+import client.core.GameProviderImpl;
+import client.core.navigation.INavigationProvider;
+import client.core.navigation.NavigationProvider;
+import client.core.socketio.SocketIOClientProvider;
+import client.core.socketio.SocketIOClientProviderImpl;
+import com.fasterxml.jackson.databind.deser.DataFormatReaders;
+import com.google.inject.AbstractModule;
+import server.configuration.ConfigurationProvider;
+import server.configuration.ConfigurationProviderImpl;
+import server.core.socketio.SocketIOServerProvider;
+import server.core.socketio.SocketIOServerProviderImpl;
+import server.core.users.ActiveUserProvider;
+import server.core.users.ActiveUserProviderImpl;
+import server.core.users.MatchmakingProvider;
+import server.core.users.MatchmakingProviderImpl;
+import storage.StorageProvider;
+import storage.StorageProviderImpl;
+
+public class DependencyModules extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(ConnectionProvider.class).to(ConnectionProviderImpl.class);
+
+        bind(INavigationProvider.class).toInstance(NavigationProvider.getInstance());
+        bind(SocketIOClientProvider.class).toInstance(SocketIOClientProviderImpl.getInstance());
+        bind(StorageProvider.class).toInstance(StorageProviderImpl.getInstance());
+        bind(ConfigurationProvider.class).toInstance(ConfigurationProviderImpl.getInstance());
+        bind(GameProvider.class).to(GameProviderImpl.class);
+
+        bind(SocketIOServerProvider.class).to(SocketIOServerProviderImpl.class);
+        bind(ActiveUserProvider.class).to(ActiveUserProviderImpl.class);
+        bind(MatchmakingProvider.class).to(MatchmakingProviderImpl.class);
+    }
+}
